e50ee4d014c0b669b7cddfa9edc06584b482570e
nickscha <nick@scharnbeck.de>
2025-08-30 12:41:15 +0200

    [VERSION] bump to v0.4

--- perf.h
+++ perf.h
@@ -1,4 +1,4 @@
-/* perf.h - v0.3 - public domain data structures - nickscha 2025
+/* perf.h - v0.4 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) simple performance profiler.
 

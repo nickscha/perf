98573757fe8c7b8fc9b35fc2e737b807c81f5d09
nickscha <nick@scharnbeck.de>
2025-08-30 11:09:18 +0200

    [VERSION] bump to v0.2

--- examples/perf_win32_nostdlib.c
+++ examples/perf_win32_nostdlib.c
@@ -1,4 +1,4 @@
-/* perf.h - v0.1 - public domain data structures - nickscha 2025
+/* perf.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) simple performance profiler.
 
